76823aefbeeb8c6ad1cf2cb6546350b71e98a2db
Arjun Radha Krishnan <arjundortmund@gmail.com>
2024-02-20 19:32:31 +0100

    Made search_jobs function to simply pass the arguments and run the search from example.py

---
+++ example.py
@@ -0,0 +1,13 @@
+
+from webscraper import search_jobs
+
+search_keyword = "deep learning, machine learning"      # The keyword to search for jobs
+search_location = "Luxembourg"                          # Preferred location of the jobs
+keywords = ["data science", "physics", "science", "scientist", "trainee", "machine learning", "deep learning"]  # Keywords for job filtering
+excluded_keywords = ["senior","head"]   # Keywords to exclude in the job title.
+languages = ["en"]                      # List here the languages of the advertisement you are interested in.
+                                        # See the documentation https://pypi.org/project/langdetect/ for abbreviations
+N_pages = 1   # No.of pages to scrape through
+
+search_jobs(search_keyword, search_location, keywords, excluded_keywords, languages, N_pages, show_window=True)
+
--- webscraper.py
+++ webscraper.py
@@ -21,92 +21,101 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
-# Set up a controllable Chrome instance in headless mode
-service = Service()
-options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")  # Uncomment to run in headless mode / without displaying the browser window
-driver = webdriver.Chrome(service=service, options=options)
+def search_jobs(search_keyword, search_location, keywords, excluded_keywords, languages=['en'], N_pages=10, show_window=False):
 
-# Open the target page in the browser
-#driver.get("https://europa.eu/europass/en/find-jobs?keyword=data&location=Germany&order=relevance&form_build_id=form-Kwm_IYWWCd_v29y--AcXtfd0Bra0I9e3xI-WTJz1K7I&form_id=jobs_search")
-driver.get("https://europa.eu/europass/en/find-jobs")
-time.sleep(2)
+    # Set up a controllable Chrome instance in headless mode
+    service = Service()
+    options = webdriver.ChromeOptions()
+    if not show_window:
+        options.add_argument("--headless=new")
+    driver = webdriver.Chrome(service=service, options=options)
 
-# Accept cookies
-cookies = driver.find_element(By.LINK_TEXT, "Accept only essential cookies")
-cookies.click()
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='cookie-consent-banner']/div/div/div[2]/button"))).click()
-
-search_keyword = "deep learning, machine learning, physics, laser, trainee"    # The keyword to search for jobs
-search_location = "netherlands"     # Preferred location of the job
-
-driver.find_element(By.XPATH,"//*[@id='edit-keyword']").send_keys(search_keyword)
-driver.find_element(By.XPATH,"//*[@id='edit-location']").send_keys(search_location)
-driver.find_element(By.CSS_SELECTOR,".search-submit").click()
-time.sleep(5)
-driver.find_element(By.CSS_SELECTOR,".search-submit").click()
-
-# Keywords for job filtering
-keywords = ["data science", "physics", "science", "scientist", "trainee", "machine learning", "deep learning", "international"]
-excluded_keywords = ["senior","head"]
-# Languages for filtering
-languages = ["en"]      # List here the languages of the advertisement you are interested in. Please see the documentation https://pypi.org/project/langdetect/ for abbreviations
-
-# Scraping logic...
-job_cards = driver.find_elements(By.CSS_SELECTOR, ".row")
-jobs = []
-positive = 0
-N_pages = 100
-
-for i, page in enumerate(tqdm(range(N_pages))):
+    # Open the target page in the browser
+    driver.get("https://europa.eu/europass/en/find-jobs")
     time.sleep(2)
-    job_cards = driver.find_elements(By.CSS_SELECTOR, ".row")
-    
-    for job in job_cards:
+
+    # Accept cookies
+    cookies = driver.find_element(By.LINK_TEXT, "Accept only essential cookies")
+    cookies.click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='cookie-consent-banner']/div/div/div[2]/button"))).click()
+
+    driver.find_element(By.XPATH,"//*[@id='edit-keyword']").send_keys(search_keyword)
+    driver.find_element(By.XPATH,"//*[@id='edit-location']").send_keys(search_location)
+    driver.find_element(By.CSS_SELECTOR,".search-submit").click()
+    time.sleep(5)
+    driver.find_element(By.CSS_SELECTOR,".search-submit").click()
+
+    jobs = []
+    positive = 0
+
+    for i_page, page in enumerate(tqdm(range(N_pages))):
+        time.sleep(2)
+        job_cards = driver.find_elements(By.CSS_SELECTOR, ".row")
+
+        for job in job_cards:
+            try:
+                # Extract job details
+                title_ele = job.find_element(By.CSS_SELECTOR, ".jobs--title")
+                title_text = title_ele.text
+                description_ele = job.find_element(By.CSS_SELECTOR, ".node-content")
+                description_text = description_ele.text
+
+                # Check if job description contains any of the keywords
+                if any(word.lower() in description_text.lower() for word in keywords) and all(word.lower() not in title_text.lower() for word in excluded_keywords) and detect(description_text) in languages:
+                    job_details = {
+                        "Title": title_text.strip(),
+                        "Description": description_text.strip(),
+                        "Link": str(job.find_element(By.CSS_SELECTOR, ".boxButtonslist a:nth-child(2)").get_attribute("href")).split('?')[0]
+                    }
+                    jobs.append(job_details)
+                    positive += 1
+                    print(str(positive) + " : " + title_text + "\n")
+
+            except Exception as e:
+                print(f"Something went wrong while fetching data: {e}")
+                pass
+
         try:
-            # Extract job details
-            title_ele = job.find_element(By.CSS_SELECTOR, ".jobs--title")
-            title_text = title_ele.text
-            description_ele = job.find_element(By.CSS_SELECTOR, ".node-content")
-            description_text = description_ele.text
-
-            # Check if job description contains any of the keywords
-            if any(word.lower() in description_text.lower() for word in keywords) and all(word.lower() not in title_text.lower() for word in excluded_keywords) and detect(description_text) in languages:
-                job_details = {
-                    "Title": title_text.strip(),
-                    "Description": description_text.strip(),
-                    "Link": str(job.find_element(By.CSS_SELECTOR, ".boxButtonslist a:nth-child(2)").get_attribute("href")).split('?')[0]
-                }
-                jobs.append(job_details)
-                positive += 1
-                print(str(positive) + " : " + title_text + "\n")
-
+            # Navigate to the next page
+            nextpage_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'li.ecl-pagination__item--next')))
+            nextpage_button.click()
         except Exception as e:
-            print(f"Something went wrong while fetching data: {e}")
-            pass
-    
-    try:
-        # Navigate to the next page
-        nextpage_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'li.ecl-pagination__item--next')))
-        nextpage_button.click()
-    except Exception as e:
-        print(f"Something went wrong while clicking next: {e}")
-        break
+            print(f"Something went wrong while clicking next: {e}")
+            break
 
-# Save results in JSON file
-with open('results/'+search_location+'_'+search_keyword+'_results.json', 'w', encoding='utf-8') as f:
-    json.dump(jobs, f, ensure_ascii=False, indent=4)
+    filename = f"{search_location}_{search_keyword}_results"
+    save_results(filename, jobs)
 
-# Save results in CSV file
-with open('results/'+search_location+'_'+search_keyword+'_results.csv', 'w', newline='', encoding='utf-8') as file:
-    csv_writer = csv.writer(file)
-    csv_writer.writerow(["Title", "URL"])
-    
-    for job in jobs:
-        csv_writer.writerow([job["Title"], f'=HYPERLINK("{job["Link"]}","{job["Link"]}")'])
+    print(f"\nFound {positive} jobs with matching keywords in {i_page+1} pages!")
 
-print(f"\nFound {positive} jobs with matching keywords in {N_pages} pages!")
+    # Close the browser and free up resources
+    driver.quit()
 
-# Close the browser and free up resources
-time.sleep(2)
-driver.quit()
+    return
+
+
+def save_results(filename, jobs):
+    # Check if the file exists
+    if os.path.exists(f"results/{filename}.json") or os.path.exists(f"{filename}.csv"):
+        # If the file exists, find a unique filename by appending a suffix
+        suffix = 1
+        while True:
+            new_filename = f"{filename}_{suffix}"
+            if not os.path.exists(f"results/{new_filename}.json"):
+                break
+            suffix += 1
+        filename = new_filename
+
+    # Save results in JSON file
+    with open(f"results/{filename}.json", 'w', encoding='utf-8') as file:
+        json.dump(jobs, file, ensure_ascii=False, indent=4)
+
+    # Save results in CSV file
+    with open(f"results/{filename}.csv", 'w', newline='', encoding='utf-8') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(["Title", "URL"])
+
+        for job in jobs:
+            csv_writer.writerow([job["Title"], f'=HYPERLINK("{job["Link"]}","{job["Link"]}")'])
+
+    return
