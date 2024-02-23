523299c98a99f781d097b9534bae06d01da9aa5d
Arjun Radha Krishnan <arjundortmund@gmail.com>
2024-02-23 11:21:11 +0100

    Update gemini_tests.py

--- gemini_tests.py
+++ gemini_tests.py
@@ -8,7 +8,12 @@ Created on Thu Feb 22 17:43:47 2024
 import pathlib
 import textwrap
 from time import time
+import os
 import google.generativeai as genai
+
+genai.configure(api_key = os.environ["GEMINI_API_KEY"])
+
+model = genai.GenerativeModel('gemini-pro')
 
 def get_response(model,message):
     response = model.generate_content(message)
@@ -31,62 +36,29 @@ def read_profile(filename):
                 
     return(profile)
 
+def profile_match(job, profile):
+    messages = [{'role':'user',
+                 'parts':[f"You are given a job description delimited by ```. \
+    Job description: ```{job}```."]},
+                {'role':'model',
+                 'parts':["ok."]}]
+                     
+    m_copies = [list(messages) for _ in range(3)]
 
-#%%    
-genai.configure(api_key="AIzaSyBnb90WJgaLqxsww2uzL0DJCpsKR-oyLbA")
+    m_copies[0].append({'role':'user',
+                     'parts':[f"Is it a {profile['level']} job? Post-doc position can be considered entry-level. Answer in 1 word, yes or no."]})
+    res1 = get_response(model,m_copies[0])
 
-model = genai.GenerativeModel('gemini-pro')
+    m_copies[1].append({'role':'user',
+                     'parts':[f"User's skills:{profile['skills']}. Does the user have more than half the skills relevant to the job? Answer in 1 word, yes or no."]})
+    res2 = get_response(model,m_copies[1])
 
-# with open('../profile.txt', 'r') as file:
-#     # Read all lines from the file and join them into a single string
-#     profile = ''.join(file.readlines())
-
-profile = read_profile("profile.txt")
+    #m_copies[2].append({'role':'user',
+    #                 'parts':[f"The user has {profile['languages']} language skills. Does that fullfil the language requirement for the job? Answer in 1 word, yes or no."]})
+    #res3 = get_response(model,m_copies[2])
     
-job_d = jobs[16]["Description"]
-
-
-#%%
-t0 = time()
-
-messages = [{'role':'user',
-             'parts':[f"You are given a job description delimited by ```. \
-Job description: ```{job_d}```."]},
-            {'role':'model',
-             'parts':["ok."]}] # ,
-
-
-m_copies = [list(messages) for _ in range(3)]
-
-m_copies[0].append({'role':'user',
-                 'parts':[f"Is it a {profile['level']} job? Post-doc position can be considered entry-level. Answer in 1 word, yes or no."]})
-res1 = get_response(model,m_copies[0])
-
-m_copies[1].append({'role':'user',
-                 'parts':[f"User's skills:{profile['skills']}. Does the user have more than half the skills relevant to the job? Answer in 1 word, yes or no."]})
-res2 = get_response(model,m_copies[1])
-
-m_copies[2].append({'role':'user',
-                 'parts':[f"The user has {profile['languages']} language skills. Does that fullfil the language requirement for the job? Answer in 1 word, yes or no."]})
-res3 = get_response(model,m_copies[2])
-
-print(time()-t0)
-
-#%%                 
-model = genai.GenerativeModel('gemini-pro')
-chat = model.start_chat(history=[])
-
-t0 = time()
-
-message = f"You are given the profile of the user delimited by ### and a job description delimited by ```. \
-Profile: ###{profile}### \n \
-Job description: ```{job_d}```. Respond in 1 word. \n"
- 
-chat_response(chat,message)
-
-res1 = chat_response(chat,"Which is the preferred level of job by the user? Does it match with the job description? Answer in 1 word, yes or no.")
-res2 = chat_response(chat,"Does the user have more than half the skills relevant to the job? Answer in 1 word, yes or no.")
-res3 = chat_response(chat,"Does the job require proficieny in any language? is the user have the required proficieny in that language? Answer in 1 word, yes or no.")
-
-t = time() - t0
-print(t)
+    results = [res1.lower(),res2.lower()] #,res3.lower()]
+    print(results)
+    verdict = all('yes' in s for s in results) 
+    
+    return(verdict)
